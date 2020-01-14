990dc54fa8c9c0c0433c2c46da5d38ce00100385
Matt Munch <mattmunch94@gmail.com>
2020-01-14 13:10:38 -0800

    back to where i started before github mishap

--- client/tableInput.js
+++ client/tableInput.js
@@ -4,7 +4,7 @@ const turnInputs = [
     type: 'list',
     name: 'turn-choice',
     message: 'It\'s your turn, choose from the following options.',
-    choices: ['table status', 'call', 'raise', 'check', 'fold']
+    choices: ['table-status', 'call', 'raise', 'check', 'fold']
   }
 ];
 
@@ -13,16 +13,16 @@ const outOfTurnInputs = [
     type: 'list',
     name: 'out-of-turn-choice',
     message: 'You may choose from the following options.',
-    choices: ['table status', 'chat', 'leave table']
+    choices: ['table-status', 'chat', 'leave-table']
   }
 ];
 
 const startHandInputs = [
   {
     type: 'list',
-    name: 'start-hand',
+    name: 'deal-hands',
     message: 'You may choose from the following options.',
-    choices: ['deal hands', 'chat', 'leave table']
+    choices: ['deal-hands']
   }
 ];
 
