bcb0546e8cd39f0e7dc073081dadb3671f1da1c1
Matt Munch <mattmunch94@gmail.com>
2020-01-13 17:44:15 -0800

    needs more work

--- client/poker.js
+++ client/poker.js
@@ -1,4 +1,21 @@
+const socket = require('socket.io-client')('http://localhost:7890');
+const { firstHandPrompt, playerTurnPrompt } = require('./tablePrompts');
+socket.on('disconnect', function() {
+  socket.emit('disconnect');
+});
+socket.on('connect', () => {
+  console.log('You\'re connected', socket.id);
+  firstHandPrompt()
+    .then(userChoice => {
+      socket.emit('userChoice', userChoice);
+      playerTurnPrompt();
+    });
+});
+socket.on('dealCards', (data) => {
+  console.log('recieved cards', data);
+});
 
-const { playerTurnPrompt } = require('./tablePrompts');
 
-playerTurnPrompt();
+// const { playerTurnPrompt } = require('./tablePrompts');
+
+// playerTurnPrompt();
--- client/tablePrompts.js
+++ client/tablePrompts.js
@@ -8,7 +8,9 @@ const playerTurnPrompt = () =>
 const playerOutOfTurnPrompt = () => 
   inquirer.prompt(outOfTurnInputs)
     .then(userChoice => userChoice);
-    
+// const proceed = () => {
+//         inquirer.prompts
+// };
 const firstHandPrompt = () => 
   inquirer.prompt(startHandInputs)
     .then(userChoice => userChoice);
