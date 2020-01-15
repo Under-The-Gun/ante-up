21c76736a2af1fdf5201c0dbdf68aec7118e9027
sethripman <sethripman@gmail.com>
2020-01-15 11:30:21 -0800

    pair programming with eve got help from dirt about mapping

--- __tests__/gameState.test.js
+++ __tests__/gameState.test.js
@@ -8,7 +8,7 @@ describe('GameState methods', () => {
 
     testGameState.board = { cardThree: 'Th', cardFour: '3h', cardFive: 'Kh', cardSix: '5h', cardSeven: '6c' };
 
-    let winningHandString = testGameState.solveHands();
+    const winningHandString = testGameState.solveHands();
     expect(winningHandString).toEqual('Three of a Kind, 6\'s');
   });
 });
