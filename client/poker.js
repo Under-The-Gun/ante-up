dadcbe8b57266f6d99361f726a4291516be34432
Matt Munch <mattmunch94@gmail.com>
2020-01-14 17:19:08 -0800

    stuff

--- client/poker.js
+++ client/poker.js
@@ -1,19 +1,15 @@
 const socket = require('socket.io-client')('http://localhost:7890');
-
-socket.on('disconnect', function() {
-  socket.emit('disconnect');
-});
-
 const { firstHandPrompt, playerTurnPrompt } = require('./tablePrompts');
+
 socket.on('disconnect', function() {
   socket.emit('disconnect');
 });
+
 socket.on('connect', () => {
   console.log('You\'re connected', socket.id);
   firstHandPrompt().then(answer => {
     socket.emit('your-hold-cards', { answer });
   });
-    
 });
 socket.on('your-hold-cards', (data) => {
   console.log('recieved cards', data);
--- client/startApp.js
+++ client/startApp.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const socket = require('socket.io-client')('http://localhost:7890');
 const { signUpPrompt, logInPrompt } = require('./userLoginSignUp');
+
 socket.on('disconnect', function() {
   socket.emit('disconnect');
 });
@@ -9,27 +10,22 @@ const startApp = [{
   type: 'list',
   name: 'start',
   message: 'Ante-Up: A Terminal Based Multiplayer Texas Holdem Game',
-  choices: ['Log In', 'Sign Up']
+  choices: ['log-in', 'sign-up']
 }];
 
-
-const pokerApp = () => {
-  socket.on('connect', () => {
-    inquirer.prompt(startApp)
-      .then(choice => {
-        switch(choice.start) {
-          case 'Log In' :
-            logInPrompt().then(pokerApp);
-            break;
-          case 'Sign Up': 
-            signUpPrompt().then(pokerApp);
-            break;
-        }
-      })
-      .catch(err => {
-        console.log('error:', err);
-      });
-  });
-};
-pokerApp();
-module.exports = { pokerApp };
+const startAppPrompt = () =>
+  inquirer.prompt(startApp)
+    .then(choice => {
+      switch(choice.start) {
+        case 'log-in' :
+          logInPrompt().then();
+          break;
+        case 'sign-up': 
+          signUpPrompt().then();
+          break;
+      }
+    })
+    .catch(err => {
+      console.log('error:', err);
+    });
+module.exports = { startAppPrompt };
