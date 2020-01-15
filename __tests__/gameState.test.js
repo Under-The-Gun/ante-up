6d5e9f7f3301cfe5fb65f9ffc18b5fdb15066463
sethripman <sethripman@gmail.com>
2020-01-15 14:22:57 -0800

    messing with pokersolver

--- __tests__/gameState.test.js
+++ __tests__/gameState.test.js
@@ -4,7 +4,7 @@ describe('GameState methods', () => {
   it('should solve hands when given multiple hands and some board cards', () => {
     let testGameState = new GameState();
 
-    testGameState.hands = { socketIdOne: { cardOne: '2s', cardTwo: '3s' }, socketIdTwo: { cardOne: '4s', cardTwo: '5s' }, socketIdThree: { cardOne: '6s', cardTwo: '6h' } };
+    testGameState.hands = { socketIdTwo: { cardOne: '4s', cardTwo: '5s' }, socketIdThree: { cardOne: '6s', cardTwo: '6h' }, socketIdOne: { cardOne: '2s', cardTwo: '3s' } };
 
     testGameState.board = { cardThree: 'Th', cardFour: '3h', cardFive: 'Kh', cardSix: '5h', cardSeven: '6c' };
 
