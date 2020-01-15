4c45de1f87e234d01cf678c6ef5a4a1515b8d17f
sethripman <sethripman@gmail.com>
2020-01-15 10:48:27 -0800

    wrote test that fails hurrah pair programming with eve

--- __tests__/gameState.test.js
+++ __tests__/gameState.test.js
@@ -3,10 +3,12 @@ const GameState = require('../lib/utils/gameState');
 describe('GameState methods', () => {
   it('should solve hands when given multiple hands and some board cards', () => {
     let testGameState = new GameState();
-    testGameState.hands = { }
 
+    testGameState.hands = { socketIdOne: { cardOne: '2s', cardTwo: '3s' }, socketIdTwo: { cardOne: '4s', cardTwo: '5s' }, socketIdThree: { cardOne: '6s', cardTwo: '6h' } };
 
-    let hand = Hand.solve(['Kh', 'Tc', '5d', 'As', '3c', '3s', '2h']);
-    expect(hand.descr).toEqual('Pair, 3\'s');
+    testGameState.board = { cardThree: 'Th', cardFour: '3h', cardFive: 'Kh', cardSix: '5h', cardSeven: '6c' };
+
+    let winningHandString = testGameState.solveHands();
+    expect(winningHandString).toEqual('Three of a Kind, 6\'s');
   });
 });
