7e568d982c24faa6146d4d8e2a47b182d48cecf5
sethripman <sethripman@gmail.com>
2020-01-17 10:47:53 -0800

    hacking colors in

--- client/cleanUpData.js
+++ client/cleanUpData.js
@@ -1,13 +1,40 @@
-const color = require('colors');
+const colors = require('colors');
+
+const colorCard = (card) => {
+  switch(card.suit) {
+    case 'HEARTS': 
+      card.visual.forEach(line => line = line.brightMagenta);
+      break;
+
+    case 'DIAMONDS': 
+      card.visual.forEach(line => line = line.brightRed);
+      break;
+
+    case 'CLUBS': 
+      card.visual.forEach(line => line = line.brightYellow);
+      break;
+
+    case 'SPADES': 
+      card.visual.forEach(line => line = line.brightGreen);
+      break;
+  }
+};
 
 const cleanUpCards = (data, type) => {
   switch(type) {
     case 'hold':
-      data.cardOne.visual.forEach((line, index) => console.log(`                     ${data.cardOne.visual[index]}`.blue + '  ' + `${data.cardTwo.visual[index]}`.red));
+      colorCard(data.cardOne);
+      colorCard(data.cardTwo);
+      data.cardOne.visual.forEach((line, index) => console.log(`                           ${data.cardOne.visual[index]}` + '  ' + `${data.cardTwo.visual[index]}`));
       break;
 
     case 'board':
-      data.cardThree.visual.forEach((line, index) => console.log(`      ${data.cardThree.visual[index]}` `${data.cardFour.visual[index]}` `${data.cardFive.visual[index]}` `${data.cardSix.visual[index]}` `${data.cardSeven.visual[index]}`));
+      colorCard(data.cardThree);
+      colorCard(data.cardFour);
+      colorCard(data.cardFive);
+      colorCard(data.cardSix);
+      colorCard(data.cardSeven);
+      data.cardThree.visual.forEach((line, index) => console.log(`      ${data.cardThree.visual[index]}` + `  ${data.cardFour.visual[index]}` + `  ${data.cardFive.visual[index]}` + `  ${data.cardSix.visual[index]}` + `  ${data.cardSeven.visual[index]}`));
       break;
 
     case 'winner':
