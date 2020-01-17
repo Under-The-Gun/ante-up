71f39a18c3b959807724a2a052891af14346922e
Eve Stockstill <evestockstillpdx@gmail.com>
2020-01-16 22:02:26 -0800

    password test

--- __tests__/socketio.test.js
+++ __tests__/socketio.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 const io = require('socket.io-client');
 // const server = require('../lib/app');
@@ -23,6 +24,9 @@ describe('login signup', () => {
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
+  beforeEach(() => {
+    return User.init();
+  });
 
   afterEach(done => {
     socket.disconnect();
@@ -31,7 +35,7 @@ describe('login signup', () => {
 
   //Use socket to emit and listen to events on the client side
 
-  it('handles bad for user that does not exist', (done) => {
+  it('handles input for  a user that does not exist', (done) => {
     socket.on('login-unsuccessful', message => {
       expect(message).toEqual('Invalid username or password!');
       done();
@@ -42,20 +46,32 @@ describe('login signup', () => {
     });
   });
 
-  // it('cant create account if username is taken', async(done) => {
-  //   jest.setTimeout(30000);
-  //   await User.create({ username: 'Danny', password: '123' })
-  //     .then(createdUser => console.log('created test user', createdUser));
+  it('can not create account if username is taken', async(done) => {
+    await User.create({ username: 'Danny', password: '123' })
+      .then(createdUser => console.log('created test user', createdUser));
 
-  //   socket.on('sign-up-unsuccessful', message => {
-  //     expect(message).toEqual('Username taken, please try again!');
-  //     done();
-  //   });
-  //   socket.emit('signup', {
-  //     username: 'Danny',
-  //     password: '12345'
-  //   });
-  // });
+    socket.on('sign-up-unsuccessful', message => {
+      expect(message).toEqual('Username taken, please try again!');
+      done();
+    });
+    socket.emit('signup', {
+      username: 'Danny',
+      password: '12345'
+    });
+  });
+
+  it('can not authorize to account if password is wrong', async(done) => {
+    await User.create({ username: 'Danny eve', password: '12345' })
+      .then(authUser=> console.log('can not auth', authUser));
+    socket.on('login-unsuccessful', message => {
+      expect(message).toEqual('Invalid username or password!');
+      done();
+    });
+    socket.emit('login', {
+      username: 'Danny',
+      passwordHash: '1234'
+    });
+  });
 
   it('can create a user account', (done) => {
     socket.on('sign-up-successful', user => {
